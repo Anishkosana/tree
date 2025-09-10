8c7964724d9e3b06ed0a9070d0d089848e10090d
Anish k <anishreddy1912@gmail.com>
2025-09-10 16:46:50 +0530

    added tree creation logic

---
+++ TreeCreation.py
@@ -0,0 +1,19 @@
+class TreeNode:
+    def __init__(self,val=0,left=None,right=None):
+        self.val=val
+        self.left=left
+        self.right=right
+
+class Tree:
+    def build_tree(self,elements,index=0):
+        if index>=len(elements):
+            return None
+        
+        node=TreeNode(elements[index])
+        node.left=self.build_tree(elements,2*index+1)
+        node.right=self.build_tree(elements,2*index+2)
+        return node
+
+elements=[1,2,3,4,5,6,7]
+tree=Tree()
+root=tree.build_tree(elements)
