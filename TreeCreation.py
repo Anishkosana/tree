0bf663b538c4c2a3d6eb47df8597035818dcde9a
Anish k <anishreddy1912@gmail.com>
2025-09-12 12:19:42 +0530

    corrected i+=1 error in the iteration version

--- TreeCreation.py
+++ TreeCreation.py
@@ -47,10 +47,10 @@ class Tree:
             if i<len(elements):
                 current.right=TreeNode(elements[i])
                 stack.append(current.right)
-        
+            i+=1
         return root
         
-elements=[1,2,3,4,5,6,7,8]
-tree=Tree()
-root=tree.build_binary_tree_iteration(elements)
+# elements=[1,2,3,4,5,6,7,8]
+# tree=Tree()
+# root=tree.build_binary_tree_iteration(elements)
 # root2=tree.build_tree_al(elements)
