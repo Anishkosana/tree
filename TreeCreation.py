639198db3fd29e407e07a96937aea13cedcb51cd
Anish k <anishreddy1912@gmail.com>
2025-09-12 11:01:11 +0530

    Added iterative version of tree building and commented the left skewed version

--- TreeCreation.py
+++ TreeCreation.py
@@ -13,20 +13,44 @@ class Tree:
         node.left=self.build_tree(elements,2*index+1)
         node.right=self.build_tree(elements,2*index+2)
         return node
-    def build_tree_al(self,elements):
+    
+    # This version only build the left skewed tree
+    
+    # def build_tree_al(self,elements):
+    #     if not elements:
+    #         return None
+        
+    #     val=elements.pop(0)
+    #     node=TreeNode(val)
+        
+    #     if elements:
+    #         node.left=self.build_tree_al(elements)
+    #     if elements:
+    #         node.right=self.build_tree_al(elements)
+    #     return node
+    
+    # iterative version of building tree
+    
+    def build_binary_tree_iteration(elements):
         if not elements:
             return None
         
-        val=elements.pop(0)
-        node=TreeNode(val)
+        root=TreeNode(elements[0])
+        stack=[root]
+        i=1
+        while i<len(elements):
+            current=stack.pop(0)
+            if i<len(elements):
+                current.left=TreeNode(elements[i])
+                stack.append(current.left)
+            i+=1
+            if i<len(elements):
+                current.right=TreeNode(elements[i])
+                stack.append(current.right)
         
-        if elements:
-            node.left=self.build_tree_al(elements)
-        if elements:
-            node.right=self.build_tree_al(elements)
-        return node
-
+        return root
+        
 elements=[1,2,3,4,5,6,7]
 tree=Tree()
 root=tree.build_tree(elements)
-root2=tree.build_tree_al(elements)
+# root2=tree.build_tree_al(elements)
