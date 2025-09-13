508d86a3855357f0f14b4f286a303d7b593ba5f4
Anish k <anishreddy1912@gmail.com>
2025-09-13 21:32:40 +0530

    uncommented exsisting lines of code

--- TreeCreation.py
+++ TreeCreation.py
@@ -50,7 +50,7 @@ class Tree:
             i+=1
         return root
         
-# elements=[1,2,3,4,5,6,7,8]
-# tree=Tree()
-# root=tree.build_binary_tree_iteration(elements)
+elements=[1,2,3,4,5,6,7,8]
+tree=Tree()
+root=tree.build_binary_tree_iteration(elements)
 # root2=tree.build_tree_al(elements)
