b17a98ac170f1c92830b2a83385d1d474bcae647
Anish k <anishreddy1912@gmail.com>
2025-09-18 14:41:19 +0530

    added methods for counting nodes!

--- TreeOperation.py
+++ TreeOperation.py
@@ -101,29 +101,60 @@ class operation:
             if node.right:
                 queue.append(node.right)
         
+    #counting nodes of a tree (recursive)
+    def count_nodes(self,root):
+        if not root:
+            return 0
         
+        return self.count_nodes(root.left)+self.count_nodes(root.right)+1
+      
+    #iterative version of counting nodes   
+    def count_n_itr(self,root):
+        if not root: return 0
         
+        stack=[root]
+        count=0  
         
-    
-
+        while stack:
+            current=stack.pop()
+            
+            if current.left:
+                stack.append(current.left)
+            if current.right:
+                stack.append(current.right)
+            
+            count+=1
+        return count
+    #counting leaf nodes
+    def leaf_nodes(self,root):
+        if not root: return 0
+        
+        if not root.left and not root.right:
+            return 1
+        
+        return self.leaf_nodes(root.left)+self.leaf_nodes(root.right)
+        
 op=operation()
 # elements=[1,2,3,4,5,6,7,8]
 # tree=tc.Tree()
 # node=tree.build_binary_tree_iteration(elements)
 
-op.inorder_itr(tc.root)
+# op.inorder_itr(tc.root)
 
-print()
+# print()
 
-op.preorder_itr(tc.root)
+# op.preorder_itr(tc.root)
 
-print()
+# print()
 
-op.postorder_itr(tc.root)
+# op.postorder_itr(tc.root)
 
-print()
+# print()
 
-op.levelorder_itr(tc.root)
+# op.levelorder_itr(tc.root)
+print(op.leaf_nodes(tc.root))
+
+
 
 
     
