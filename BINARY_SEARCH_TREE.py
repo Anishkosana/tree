40def747d7e3b2d48223cad8ba1d683bf068d930
Anish k <anishreddy1912@gmail.com>
2025-09-19 11:40:17 +0530

    iterative method for creating BST(mistaken the first commit)

--- BINARY_SEARCH_TREE.py
+++ BINARY_SEARCH_TREE.py
@@ -4,3 +4,39 @@ class Node:
         self.left=left
         self.right=right
         
+class BST:
+    
+    # iterative method of building bst
+    def insert(self,root,key):
+        
+        if not root:
+            return Node(key)
+        
+        curr=root
+        parent=None
+        
+        while curr:
+            parent=curr
+            
+            if curr.val<key:
+                curr=curr.left
+            
+            else:
+                curr=curr.right
+                
+        if key<parent.val:
+            parent.left=Node(key)
+        
+        else:
+            parent.right=Node(key)
+            
+        return root
+        
+    def build(self,nums):
+        
+        root=None
+        
+        for num in nums:
+            root=self.insert(root,num)
+        
+        return root
