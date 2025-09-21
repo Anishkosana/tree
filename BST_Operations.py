406b4b0c70db6663e4e886fe1a469a61cd614c6f
Anish k <anishreddy1912@gmail.com>
2025-09-21 10:19:29 +0530

    recursive and iterative insert for single element

--- BST_Operations.py
+++ BST_Operations.py
@@ -29,8 +29,48 @@ class BST_OPERATION:
         if key<root.val:
             return self.RSearch(root.left,key)
         else:
-            return self.RSearch(root.right,key)           
+            return self.RSearch(root.right,key) 
+        
+    # Inorder Traversal
+    def Inorder(self,root):
+        if not root: return None
+        
+        self.Inorder(root.left)
+        print(root.val,end="->")
+        self.Inorder(root.right)  
+    
+    #Iterative insert
+    def IInsert(self,root,key):
+        r=None
+        t=root
+        while t:
+            r=t
+            if key==t.val:return
+            
+            elif key<t.val:t=t.left
+            else:t=t.right
+        
+        n=bst.Node(key)
+        if n.val<r.val:r.left=n 
+        else: r.right=n
+    
+    #Recursive insert
+    def RInsert(self,root,key):
+        if not root:
+            return bst.Node(key)
+        if root.val==key:
+            return None
+        if key<root.val:
+            root.left=self.RInsert(root.left,key)
+        else:
+            root.right=self.RInsert(root.right,key)
+        
+        return root
         
 op=BST_OPERATION()
-print(op.ISearch(bst.root,7))
-print(op.RSearch(bst.root,7))
+# print(op.ISearch(bst.root,7))
+# print(op.RSearch(bst.root,7))
+
+# op.IInsert(bst.root,2)
+op.RInsert(bst.root,2)
+op.Inorder(bst.root)
